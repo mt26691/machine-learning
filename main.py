8f2a8f2092b0b0451615a4d3e78a73c3439055e0
mt26691 <tung.nguyen@scopicsoftware.com>
2019-08-10 10:43:17 +0700

    read and save csv file

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 import pandas as pd
+
+df = pd.read_csv("./data/imports-85.csv", header=None)
+
+df.columns = [
+    "symboling",
+    "normalized-losses",
+    "make",
+    "fuel-type",
+    "aspiration",
+    "num-of-doors",
+    "body-style",
+    "drive-wheels",
+    "engine-location",
+    "wheel-base",
+    "length",
+    "width",
+    "height",
+    "curb-weight",
+    "Engine type",
+    "num-of-cylinders",
+    "engine-size",
+    "fuel-system",
+    "bore",
+    "stroke",
+    "compression-ratio",
+    "horsepower",
+    "peak-rpm",
+    "city-mpg",
+    "highway-mpg",
+    "price",
+]
+print(df.head(5))
+
+df.to_csv("./data/changed-85.csv")
