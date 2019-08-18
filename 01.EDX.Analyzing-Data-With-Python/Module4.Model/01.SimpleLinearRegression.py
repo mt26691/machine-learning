ba0deaf7876b7affecc135fc9e8323cb8b842660
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-08-18 15:19:28 +0700

    simple linear regression

--- 01.EDX.Analyzing-Data-With-Python/Module4.Model/01.SimpleLinearRegression.py
+++ 01.EDX.Analyzing-Data-With-Python/Module4.Model/01.SimpleLinearRegression.py
@@ -4,31 +4,28 @@ import matplotlib as plt
 from matplotlib import pyplot
 import seaborn as sns
 from scipy import stats
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("./data/automobileEDA.csv")
 
+lm =LinearRegression()
 
-print(df[['wheel-base', 'price']].corr())
+# define predictor variable and target variable
+X = df[['highway-mpg']]
+Y = df['price']
 
-pearson_coef, p_value = stats.pearsonr(df['wheel-base'], df['price'])
-print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+# use lm.fit to fit the model and then obtain the prediction
+lm.fit(X,Y)
 
-pearson_horse_power_coef, p_horse_power_value = stats.pearsonr(df['horsepower'], df['price'])
+Yhat = lm.predict(X)
 
-# conclusion
-# the linear relationship (Pearson correlation) is quite strong (0.8095) that is close to +1
-# the p value is very small (< 0.001), so we have a strong, certainly in the result
-print("The Pearson Correlation Coefficient between horsepower and price is", pearson_horse_power_coef, " with a P-value of P =", p_horse_power_value)
+# the intercept(b0)
+print('The intercept is ', lm.intercept_)
+print('the slope is ', lm.coef_)
 
-pearson_length_coef, p_length_value = stats.pearsonr(df['length'], df['price'])
-# conclusion
-# the linear relationsip is 0.69, so we may have linear here,
-# the P value is <0.001 so the correlation between length and price is statistically significant.
-print("The Pearson Correlation Coefficient (length and price) is", pearson_length_coef, " with a P-value of P = ", p_length_value)  
+# the relationship between highway mpg and price is
+# Pirce = 38423.30 - 821.733*(highway-mpg)
 
-pearson_width_coef, p_width_value = stats.pearsonr(df['width'], df['price'])
-print("The Pearson Correlation Coefficient (width and price) is", pearson_width_coef, " with a P-value of P = ", p_width_value)  
+Yhat = lm.predict(X)
 
-
-pearson_curb_weight_coef, p_curb_weight_value = stats.pearsonr(df['curb-weight'], df['price'])
-print("The Pearson Correlation Coefficient (curb-weight and price) is", pearson_curb_weight_coef, " with a P-value of P = ", p_curb_weight_value)  
+print(Yhat)
