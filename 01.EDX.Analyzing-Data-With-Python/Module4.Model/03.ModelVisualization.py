e56ebc9bbf9255f0cd75a18275bdc0791334170a
Nguyen Manh Tung <tung.nguyen@scopicsoftware.com>
2019-08-18 16:15:55 +0700

    regression + residual plot

--- 01.EDX.Analyzing-Data-With-Python/Module4.Model/03.ModelVisualization.py
+++ 01.EDX.Analyzing-Data-With-Python/Module4.Model/03.ModelVisualization.py
@@ -8,5 +8,11 @@ from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("./data/automobileEDA.csv")
 
+# regression plot
+sns.regplot(x='highway-mpg', y='price', data = df)
+pyplot.ylim(0,)
+pyplot.show()
+
+#residual plot
 sns.residplot(df['highway-mpg'], df['price'])
 pyplot.show()
